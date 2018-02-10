eca82f3014fc8c7bc83a6f6820c9575b89d45900
Lukas Lukac <services@trki.sk>
2018-02-10 19:27:33 +0100

    Adds my 'inner-circle' SpringBoot application.

---
+++ inner-circle/src/main/java/com/inner_circle/InnerCircleApplication.java
@@ -0,0 +1,33 @@
+package com.inner_circle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+@Controller
+@SpringBootApplication
+public class InnerCircleApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() throws InterruptedException {
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat calendarFormat = new SimpleDateFormat("HH:mm:ss");
+
+		System.out.printf("Request received at: %s.\n", calendarFormat.format(calendar.getTime()));
+		System.out.printf("Thread name: %s.\n", Thread.currentThread().getName());
+
+		Thread.sleep(10000);
+
+		return "A heroic path from PHP to Java!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnerCircleApplication.class, args);
+	}
+}
---
+++ inner-circle/src/test/java/com/inner_circle/InnerCircleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.inner_circle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InnerCircleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
